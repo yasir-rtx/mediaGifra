c4fd73cfa487e5754621a0cde47ac875510ded32
yasir <yelfvankarlenstain@gmail.com>
2022-06-11 17:40:15 +0700

    Fixing database iption value

--- evaluasi/db.js
+++ evaluasi/db.js
@@ -2,30 +2,30 @@ var data = [
     {
         "qs": "1. Harga 3 celana dan 2 baju adalah Rp280.000,00. sedangkan 1 celana dan 3 baju seharga Rp210.00,00. Model matematika dari persoalan tersebut adalah ...",
         "options": [
-            "1",
-            "2",
-            "3",
-            "4"
+            " A. 3c + 2b = Rp280.000,00 dan &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 3c + c = Rp210.000,00",
+            " B. 3c + 2b = Rp280.000,00 dan &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; c + 3b = Rp210.000,00",
+            " C. 3c - 2b = Rp210.000,00 dan &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; c + 2b = Rp280.000,00",
+            " D. 3c + 2b = Rp210.000,00 dan &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; c + 2b = Rp280.000,00"
         ],
         "answerId": 1
     },
     {
         "qs": "Harga 5 buah kue A dan 2 buah kue B Rp4.000,00. sedangkan harga 2 buah kue A dan harga 3 buah kue B Rp2.700,00. jadi, harga sebuah kue A dan kue B adalah ...",
         "options": [
-            "1",
-            "2",
-            "3",
-            "4"
+            " A. Rp1.100,00",
+            " B. Rp1.600,00",
+            " C. Rp1.800,00",
+            " D. Rp2.400,00"
         ],
-        "answerId": 2
+        "answerId": 0
     },
     {
         "qs": "Jika 2x - 3y = 7 dan 3x + 2y = 4, maka nilai x + y adalah ...",
         "options": [
-            "1",
-            "2",
-            "3",
-            "4"
+            " A. -3",
+            " B. -1",
+            " C. 1",
+            " D. 3"
         ],
         "answerId": 3
     },
