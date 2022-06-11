c9594d0681543868f84857505df47b1f7b451bbf
yasir <yelfvankarlenstain@gmail.com>
2022-06-11 14:47:55 +0700

    Evaluasi selesai

---
+++ evaluasi/db.js
@@ -0,0 +1,32 @@
+var data = [
+    {
+        "qs": "1. Harga 3 celana dan 2 baju adalah Rp280.000,00. sedangkan 1 celana dan 3 baju seharga Rp210.00,00. Model matematika dari persoalan tersebut adalah ...",
+        "options": [
+            "1",
+            "2",
+            "3",
+            "4"
+        ],
+        "answerId": 1
+    },
+    {
+        "qs": "Harga 5 buah kue A dan 2 buah kue B Rp4.000,00. sedangkan harga 2 buah kue A dan harga 3 buah kue B Rp2.700,00. jadi, harga sebuah kue A dan kue B adalah ...",
+        "options": [
+            "1",
+            "2",
+            "3",
+            "4"
+        ],
+        "answerId": 2
+    },
+    {
+        "qs": "Jika 2x - 3y = 7 dan 3x + 2y = 4, maka nilai x + y adalah ...",
+        "options": [
+            "1",
+            "2",
+            "3",
+            "4"
+        ],
+        "answerId": 3
+    },
+]
