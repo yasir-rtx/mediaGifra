f1961e56586bc8fa476e78cb8ce6761139ce3bb3
yasir <yelfvankarlenstain@gmail.com>
2022-06-12 20:28:59 +0700

    fungsi send to whatsapp 100% Done

---
+++ whatsapp/des.php
@@ -0,0 +1,15 @@
+<?php
+    if (isset($_POST['submit'])) {
+        $name = $_POST['name'];
+        $message = $_POST['message'];
+        $noWa = $_POST['noWa'];
+
+        header("location:https://api.whatsapp.com/send?phone=6281363015280&text=Nama:%20$name%20%0aPesan:%20$message");
+    } else {
+        echo "
+            <script>
+                window.location=history.go(-1);
+            </script>
+        ";
+    }
+?>
