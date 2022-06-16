c29e7f9cb80469973f7a3f647b309176d066f092
gifra_kiting <gifrarasyidah@gmail.com>
2022-06-16 21:19:53 +0700

    Menambah soal di evaluasi dan cek jika jawaban kosong

--- evaluasi/db.js
+++ evaluasi/db.js
@@ -10,7 +10,37 @@ var data = [
         "answerId": 1
     },
     {
-        "qs": "Harga 5 buah kue A dan 2 buah kue B Rp4.000,00. sedangkan harga 2 buah kue A dan harga 3 buah kue B Rp2.700,00. jadi, harga sebuah kue A dan kue B adalah ...",
+        "qs": "2. Titik potong pada sumbu x untuk persamaan x + y = 7 adalah ...",
+        "options": [
+            " A. (0,7)",
+            " B. (7,0)",
+            " C. (0,-7)",
+            " D. (-7,0)"
+        ],
+        "answerId": 0
+    },
+    {
+        "qs": "3. Titik potong pada sumbu y untuk persamaan x - y = 7 adalah ...",
+        "options": [
+            " A. (0,7)",
+            " B. (7,0)",
+            " C. (0,-7)",
+            " D. (-7,0)"
+        ],
+        "answerId": 2
+    },
+    {
+        "qs": "4. Jika 3x + y = -1 dan x + 3y = 5, maka penyelesaian dari SPLDV tersebut adalah ...",
+        "options": [
+            " A. (1,-2)",
+            " B. (2,-1)",
+            " C. (-1,2)",
+            " D. (-2,1)"
+        ],
+        "answerId": 2
+    },
+    {
+        "qs": "5. Harga 5 buah kue A dan 2 buah kue B Rp4.000,00. sedangkan harga 2 buah kue A dan harga 3 buah kue B Rp2.700,00. jadi, harga sebuah kue A dan kue B adalah ...",
         "options": [
             " A. Rp1.100,00",
             " B. Rp1.600,00",
@@ -20,7 +50,27 @@ var data = [
         "answerId": 0
     },
     {
-        "qs": "Jika 2x - 3y = 7 dan 3x + 2y = 4, maka nilai x + y adalah ...",
+        "qs": "6. Jika 2x - 3y = 7 dan 3x + 2y = 4, maka nilai x + y adalah ...",
+        "options": [
+            " A. -3",
+            " B. -1",
+            " C. 1",
+            " D. 3"
+        ],
+        "answerId": 3
+    },
+    {
+        "qs": "7. Jika a + b = 3 dan a - b = 1, maka nilai 2a adalah ...",
+        "options": [
+            " A. -1",
+            " B. 1",
+            " C. 2",
+            " D. 4"
+        ],
+        "answerId": 3
+    },
+    {
+        "qs": "8. Jika 3x - y = 15 dan x + 3y = 3, maka hasil dari x - 2y adalah ...",
         "options": [
             " A. -3",
             " B. -1",
